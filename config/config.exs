3567d237e21434a89ded9e080834e8e7f341a8d1
Manuel Familia <manuelgfx@gmail.com>
2018-07-24 13:13:48 -0400

    Add api as an application under the umbrella

--- config/config.exs
+++ config/config.exs
@@ -9,6 +9,7 @@ use Mix.Config
 # back to each application for organization purposes.
 #import_config "../apps/ui/config/config.exs"
 import_config "../apps/core/config/config.exs"
+import_config "../apps/api/config/config.exs"
 import_config "../apps/fw/config/config.exs"
 
 # Sample configuration (overrides the imported configuration above):
